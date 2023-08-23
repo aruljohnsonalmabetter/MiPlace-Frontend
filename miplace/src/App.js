406c66212322e723e534b065cf65423b3f505495
Rudransh <rudrcodes@gmail.com>
2023-08-24 00:10:13 +0530

    Basic changes

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import "./App.css";
 import { FooterComponent } from "./components/FooterComponent";
 import { HeaderComponent } from "./components/HeaderComponent";
-=======
 //navbar 
 //basic structure
 //
@@ -10,21 +8,17 @@ import { HeaderComponent } from "./components/HeaderComponent";
 import './App.css';
 import Testimonial from './components/Testimonial.jsx';
 
->>>>>>> a3c45845122f0a5c035080bd34051c8feb9f0ec9
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
       <header className="App-header">
-<<<<<<< HEAD
        Body
-=======
         <h1 className="text-3xl font-bold underline"> 
          Hello world!
         </h1>
 
         <Testimonial/>
->>>>>>> a3c45845122f0a5c035080bd34051c8feb9f0ec9
       </header>
       <FooterComponent/>
     </div>
