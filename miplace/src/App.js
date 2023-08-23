70dfb4c34edc99abb8d01918cefcf85e5b9beb2d
Rudransh <rudrcodes@gmail.com>
2023-08-23 14:53:46 +0530

    Started working on Header and Footer -> Rudransh

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
+import { FooterComponent } from "./components/FooterComponent";
+import { HeaderComponent } from "./components/HeaderComponent";
 function App() {
   return (
     <div className="App">
+      <HeaderComponent/>
       <header className="App-header">
         <h1 className="text-2xl font-bold underline text-orange-600	">
           Tailwind Setup Done
         </h1>
         <p>Hello lets start our miplace</p>
       </header>
+      <FooterComponent/>
     </div>
   );
 }
---
+++ miplace/src/components/FooterComponent.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export const FooterComponent = () => {
+    return (
+        <div className='p-5 navbar flex justify-center items-center top-0 sticky bg-white'>
+            <div className=' flex justify-between items-center  w-11/12 h-full	'>
+                <div className='left w-3/5 text-start	bg-green-400'>
+                    <p>About MiPlace</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum aliquet accumsan porta lectus ridiculus in mattis. Netus sodales in volutpat ullamcorper amet adipiscing fermentum.</p>
+                    <ul className='bg-red-800 text-start'>
+                        <a href='http://localhost:3000/'>i</a>
+                        <a href='http://localhost:3000/'>i</a>
+                        <a href='http://localhost:3000/'>i</a>
+                        <a href='http://localhost:3000/'>i</a>
+                    </ul>
+                </div>
+                <div className='right flex justify-between items-center bg-red-600 '>
+                    <div >
+                        <p>Company</p>
+                        <ul className='flex flex-col justify-center items-center '>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Company</p>
+                        <ul className='flex flex-col justify-center items-center '>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Company</p>
+                        <ul className='flex flex-col justify-center items-center '>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                            <a href='http://localhost:3000/'>Instagram</a>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
