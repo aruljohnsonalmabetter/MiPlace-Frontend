eb9dcb3446e187256d34e3e6a48843528dfda0a4
Rudransh <rudrcodes@gmail.com>
2023-08-23 13:52:14 +0530

    Setup done for Tailwind

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello lets start our miplace
-        </p>
-        
+        <h1 className="text-2xl font-bold underline text-orange-600	">
+          Tailwind Setup Done
+        </h1>
+        <p>Hello lets start our miplace</p>
       </header>
     </div>
   );
