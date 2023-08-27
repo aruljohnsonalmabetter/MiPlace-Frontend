04d5ca7ac16ed9b33e03016dfbf3ae211827e710
[Arul Johnson] <arul.johnson@almabetter.com>
2023-08-27 15:19:49 +0530

    checking conflicts

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,15 +1,8 @@
-
 import "./App.css";
 import { FooterComponent } from "./components/FooterComponent";
 import { HeaderComponent } from "./components/HeaderComponent";
-<<<<<<< HEAD
 import {Popularplace} from "./components/Popularplace"
-//navbar 
-=======
-//navbar
->>>>>>> cbb206c4d19a3c85cbce80bde41bda93cf022ebe
-//basic structure
-//
+
 
 import "./App.css";
 import Testimonial from "./components/Testimonial.jsx";
@@ -17,7 +10,7 @@ import Testimonial from "./components/Testimonial.jsx";
 import HomeCard from "./components/HomeCard";
 
 import { MostSearchedLocations } from "./components/MostSearchedLocations";
-import { PlaceSearchBarComponent } from "./components/PlaceSearchBarComponent";
+// import { PlaceSearchBarComponent } from "./components/PlaceSearchBarComponent";
 
 function App() {
   return (
@@ -25,9 +18,7 @@ function App() {
       <HeaderComponent />
       <HomeCard />
 
-<<<<<<< HEAD
-      <HeaderComponent/>      
-      <HomeCard/>
+      
 
       
       <MostSearchedLocations/>
@@ -36,12 +27,6 @@ function App() {
   
 
       <FooterComponent/>
-=======
-      <MostSearchedLocations />
-      <Testimonial />
-
-      <FooterComponent />
->>>>>>> cbb206c4d19a3c85cbce80bde41bda93cf022ebe
     </div>
   );
 }
