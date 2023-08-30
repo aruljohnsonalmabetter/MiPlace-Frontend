83aeb269df1487abcad3de0fa8a8e4428cb0d46c
Rudransh <rudrcodes@gmail.com>
2023-08-30 23:48:00 +0530

    Created the Write Review Page

--- miplace/src/components/HeaderComponent.js
+++ miplace/src/components/HeaderComponent.js
@@ -1,39 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-
- const  HeaderComponent = () => {
-        return (
-            <div className='z-10 navbar flex justify-center items-center h-20 top-0 sticky bg-white'>
-                <div className='flex justify-between items-center w-11/12 h-full'>
-                    <div className="logo font-bold text-xl">
-                        <h2>MiPlace</h2>
-                    </div>
-                    <div className="w-2/5 text-lg">
-                        <ul className='flex justify-between items-center'>
-                            <li>
-                                About Us
-                            </li>
-                            <li>
-                                Contact Us
-                            </li>
-                            <li>
-                                {/* Use Link to navigate to the login page */}
-                                <Link to="/login" className='btn px-6 py-3 rounded-md'>
-                                    Login
-                                </Link>
-                            </li>
-                            <li>
-                                {/* Use Link to navigate to the sign-up page */}
-                                <Link to="/signup" className='btn px-6 py-3 rounded-md'>
-                                    Sign Up
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-    );
+const HeaderComponent = () => {
+  return (
+    <div className="z-10 navbar flex justify-center items-center h-20 top-0 sticky bg-white">
+      <div className="flex justify-between items-center w-11/12 h-full">
+        <div className="logo font-bold text-xl">
+          <Link to="/" >
+            <h2>MiPlace</h2>
+          </Link>
+        </div>
+        <div className="w-2/5 text-lg">
+          <ul className="flex justify-between items-center">
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>
+              {/* Use Link to navigate to the login page */}
+              <Link to="/login" className="btn px-6 py-3 rounded-md">
+                Login
+              </Link>
+            </li>
+            <li>
+              {/* Use Link to navigate to the sign-up page */}
+              <Link to="/signup" className="btn px-6 py-3 rounded-md">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HeaderComponent;
