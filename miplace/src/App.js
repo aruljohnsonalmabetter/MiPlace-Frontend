61f27f019779009537c2a8d071777a68a063bfc7
[Arul Johnson] <arul.johnson@almabetter.com>
2023-08-30 23:49:17 +0530

    added bookinginfoand bill,mybooking,emailcontactusand customersupport pages and styling is left

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -9,6 +9,10 @@ import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import Login from './components/LoginFormComponent';
 import OTP from './pages/OTP'
+import BookingInfoandBill from './pages/BookingInfoandBill'
+import MyBookings from './pages/MyBookings'
+import EmailUsContactUs from './pages/EmailUsContactUs'
+import CustomerSupport from './pages/CustomerSupport'
 function App() {
   return (
 
@@ -20,6 +24,11 @@ function App() {
         <Route path="/hotelinfo" element={<HotelInfo />} />
         <Route path="/login" element={<Login />} />
         <Route path="/otp" element={<OTP />} />
+        <Route path="/bookingInfoandBill" element={<BookingInfoandBill/>} />
+        <Route path="/mybookings" element={<MyBookings />} />
+        <Route path="/emailuscontactus" element={<EmailUsContactUs />} />
+        <Route path="/customersupport" element={<CustomerSupport />} />
+
       </Routes>
      <FooterComponent/>
     </BrowserRouter>
--- miplace/src/pages/BookingInfoandBill.js
+++ miplace/src/pages/BookingInfoandBill.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function BookingInfoandBill() {
   return (
-    <div>BookingInfoandBill</div>
+    <div class="flex mx-auto w-[80rem] h-[80rem]">
+      <div class="bg-red-600 mx-auto w-[49.375rem] h-[64.25rem]">
+        <h1>hello</h1>
+      </div>
+      <div class=" mx-auto w-[28.125rem] h-[29.34375rem] border-black bg-slate-300">
+      <h1>hello</h1>
+      </div>
+      
+    </div>
   )
 }
 
