c15fed96dcd9b20026dcc3de8e0d3d7f48da730b
[Arul Johnson] <arul.johnson@almabetter.com>
2023-08-30 17:31:28 +0530

    nn

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import React from 'react';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import HotelInfo from './pages/HotelInfo'
 function App() {
   return (
-      <BrowserRouter>
+    <Router>
         <Routes>
 
         <Route index element={<HomePage />} />
         <Route path="/home" element={<HomePage/>}/>
         <Route path="/hotelinfo" element={<HotelInfo />} />
         </Routes>
-      </BrowserRouter>
+        </Router>
      
     
   );
