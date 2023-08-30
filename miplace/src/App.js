fbd2417a2bdd5aaaa6d1341dbd8ccf9e8d1879b2
[Arul Johnson] <arul.johnson@almabetter.com>
2023-08-30 21:22:06 +0530

    conflicts

--- miplace/src/App.js
+++ miplace/src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import React from 'react';
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
->>>>>>> 2a6e0b9e46cf7d3ffa8848a05323453bc60e9ac2
 import HomePage from './pages/Home';
 import HotelInfo from './pages/HotelInfo'
 import HeaderComponent from './components/HeaderComponent';
@@ -13,11 +12,6 @@ import Login from './components/LoginFormComponent';
 import OTP from './pages/OTP'
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-        <Routes>
-=======
->>>>>>> 2a6e0b9e46cf7d3ffa8848a05323453bc60e9ac2
 
     <BrowserRouter>
       <HeaderComponent/>
@@ -25,12 +19,6 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/hotelinfo" element={<HotelInfo />} />
-<<<<<<< HEAD
-        </Routes>
-        </Router>
-     
-    
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/otp" element={<OTP />} />
       </Routes>
@@ -38,7 +26,6 @@ function App() {
     </BrowserRouter>
 
 
->>>>>>> 2a6e0b9e46cf7d3ffa8848a05323453bc60e9ac2
   );
 }
 
