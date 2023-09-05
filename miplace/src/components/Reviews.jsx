13b7fc4b2d8665057f9a28c5ed43170dbda28287
Sejal Goyal <sejalgoyal661@gmail.com>
2023-09-05 17:14:54 +0530

    bookingslice and hotelslice added

--- miplace/src/components/Reviews.jsx
+++ miplace/src/components/Reviews.jsx
@@ -52,7 +52,7 @@ const reviews = [
 
 const ReviewsComponent = () => {
     return (
-        <div className="max-w-3xl mx-auto p-4">
+        <div className="w-4xl mx-auto p-4">
             <div className="w-1407px h-740px p-0 space-y-4 overflow-y-scroll">
                 <div className="flex items-center justify-between mb-4">
                     <h1 className="text-2xl font-bold">Reviews</h1>
---
+++ miplace/src/redux-features/hotelSlice.js
@@ -0,0 +1,32 @@
+// hotelSlice.js
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    hotels: [],
+    loading: false,
+    error: null,
+};
+
+const hotelSlice = createSlice({
+    name: 'hotels',
+    initialState,
+    reducers: {
+        fetchHotelsStart(state) {
+            state.loading = true;
+        },
+        fetchHotelsSuccess(state, action) {
+            state.hotels = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        fetchHotelsFailure(state, action) {
+            state.loading = false;
+            state.error = action.payload;
+        },
+    },
+});
+
+export const { fetchHotelsStart, fetchHotelsSuccess, fetchHotelsFailure } = hotelSlice.actions;
+
+export default hotelSlice.reducer;
