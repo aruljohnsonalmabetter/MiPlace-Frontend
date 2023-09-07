8ae0d0d7185da34207e9fbcdeb450c5da8897fbd
Rudransh <rudrcodes@gmail.com>
2023-09-07 19:26:00 +0530

    Added the hotel details to all the pages

--- miplace/src/components/HotelNamebooking.js
+++ miplace/src/components/HotelNamebooking.js
@@ -7,24 +7,24 @@ import { BsFillShareFill } from "react-icons/bs";
 import Rating from "./Rating";
 
 
-const HotelNamebooking = ({hotelname,city,price,text}) => {
+const HotelNamebooking = ({hotelname,city,price,text,review,address}) => {
   return (
     
     <div class="flex justify-between mx-[3.125rem]  space-x-4 border-solid border-2 border-indigo-600">
             <div>
                 <h1 className="font-[Montserrat] text-[2.125rem] font-semibold">{hotelname}</h1>
                 
-                <h3 className=" flex font-[Montserrat]  font-medium" ><BiSolidLocationPlus/> {city}</h3>
+                <h3 className=" flex font-[Montserrat]  font-medium" ><BiSolidLocationPlus/> {address}</h3>
 
                 <div className='flex'>
                       <Rating value="5" className="inline " />
 
-                      <p>review</p>
+                      <p>{review || "5"}</p>
                 </div>
                 
             </div>
             <div>
-                <h1 className="font-[Montserrat] text-[2.125rem] font-bold text-[#1E91B6]">{price}/night</h1>
+                <h1 className="font-[Montserrat] text-[2.125rem] font-bold text-[#1E91B6]">₹{price}/night</h1>
                 <div className='flex'>
                     <FcLike className="text-2xl hover:scale-75   cursor-pointer transition ease-in-out delay-250"/>
                     <BsFillShareFill/>
