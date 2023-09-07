5634abed2eddbb6445617516fe2ae63a601f3baf
Sejal Goyal <sejalgoyal661@gmail.com>
2023-09-07 18:06:58 +0530

    some responsive changes added

--- miplace/src/components/MostSearchedLocations.js
+++ miplace/src/components/MostSearchedLocations.js
@@ -31,11 +31,11 @@ export const MostSearchedLocations = () => {
   return (
     <div className=" flex justify-center items-center ">
       <div className="    mostSearchedPlaces w-10/12 ">
-        <p className="text-start text-2xl font-bold">Most Searched Locations</p>
-        <div className="  flex justify-between items-center w-full my-5">
+        <p className="text-start text-2xl font-bold text-center ">Most Searched Locations</p>
+        <div className="flex justify-between items-center w-full my-5">
           {placesObj.map((elem) => {
             return (
-              <div className="flex justify-evenly items-center mr-16 " key={Math.random() * 1000}>
+              <div className="flex justify-evenly items-center mr-16" key={Math.random() * 1000}>
                 <img className="rounded-xl" src={elem.url} alt="img" />
                 <div className="ml-3 text-start ">
                   <p className="text-lg font-semibold">{elem.placeName}</p>
