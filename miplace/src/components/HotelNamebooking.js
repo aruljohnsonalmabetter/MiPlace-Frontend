a7804de5ddcbdbaba412f8a5c2bcc6e3d89ff58e
Sejal Goyal <sejalgoyal661@gmail.com>
2023-09-08 09:30:33 +0530

    hotelinfo made responsive

--- miplace/src/components/HotelNamebooking.js
+++ miplace/src/components/HotelNamebooking.js
@@ -5,6 +5,7 @@ import { BiSolidLocationPlus } from "react-icons/bi";
 import { FcLike } from "react-icons/fc";
 import { BsFillShareFill } from "react-icons/bs";
 import Rating from "./Rating";
+import "./styles.css";
 
 
 const HotelNamebooking = ({hotelname,city,price,text}) => {
@@ -23,9 +24,9 @@ const HotelNamebooking = ({hotelname,city,price,text}) => {
                 </div>
                 
             </div>
-            <div className='gap-2 px-1.5'>
-                <h1 className="font-[Montserrat] text-[2.125rem] font-bold text-[#1E91B6]">{price}/night</h1>
-                <div className='flex gap-2' >
+            <div  className='gap-2 px-1.5'>
+              <h1 id="nighthead" className="font-[Montserrat] text-[2.125rem] font-bold text-[#1E91B6]">{price}/night</h1>
+                <div id="nighticon" className='flex gap-2' >
                     <FcLike className="text-2xl hover:scale-75   cursor-pointer transition ease-in-out delay-250"/>
                     <BsFillShareFill/>
                     <Button
