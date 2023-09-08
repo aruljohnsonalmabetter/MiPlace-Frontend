c9118ee4c9706bd52d8c3c52db4f76aea48b6aa7
Sejal Goyal <sejalgoyal661@gmail.com>
2023-09-08 17:33:06 +0530

    changes added

--- miplace/src/components/Popularplace.jsx
+++ miplace/src/components/Popularplace.jsx
@@ -9,9 +9,35 @@ export const Popularplace = () => {
         
         <div class="flex justify-center items-center ">
 
+
         
-        
-        <div class="p-6 flex flex-wrap items-center justify-center">
+          <div class="p-6 flex flex-wrap items-center justify-center">
+
+
+            <div class="flex-shrink-0 m-6 relative overflow-hidde rounded-lg max-w-xs shadow-lg">
+
+              <div class="relative  flex items-center justify-center">
+                <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
+                <img
+                  src={image}
+                  alt="" />
+
+              </div>
+              <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
+                <span class="block opacity-75 -mb-1">Indoor</span>
+                <div class="flex justify-between">
+                  <span class="block font-semibold text-xl">Peace Lily</span>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Book Now
+                  </button>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="p-12 flex flex-wrap items-center justify-center">
             
 
             <div class="flex-shrink-0 m-6 relative overflow-hidde rounded-lg max-w-xs shadow-lg">
@@ -27,7 +53,9 @@ export const Popularplace = () => {
                 <span class="block opacity-75 -mb-1">Indoor</span>
                 <div class="flex justify-between">
                   <span class="block font-semibold text-xl">Peace Lily</span>
-                  <span class=" bg-black rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Book Now
+                  </button>
                 </div>
               </div>
             </div>
@@ -51,31 +79,9 @@ export const Popularplace = () => {
                 <span class="block opacity-75 -mb-1">Indoor</span>
                 <div class="flex justify-between">
                   <span class="block font-semibold text-xl">Peace Lily</span>
-                  <span class=" bg-black rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
-                </div>
-              </div>
-            </div>
-            
-            
-          </div>
-
-          <div class="p-12 flex flex-wrap items-center justify-center">
-            
-
-            <div class="flex-shrink-0 m-6 relative overflow-hidde rounded-lg max-w-xs shadow-lg">
-              
-              <div class="relative  flex items-center justify-center">
-                <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
-                <img 
-                src={image}
-                 alt=""/>
-                 
-              </div>
-              <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
-                <span class="block opacity-75 -mb-1">Indoor</span>
-                <div class="flex justify-between">
-                  <span class="block font-semibold text-xl">Peace Lily</span>
-                  <span class=" bg-black rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Book Now
+                  </button>
                 </div>
               </div>
             </div>
@@ -100,7 +106,9 @@ export const Popularplace = () => {
                 <span class="block opacity-75 -mb-1">Indoor</span>
                 <div class="flex justify-between">
                   <span class="block font-semibold text-xl">Peace Lily</span>
-                  <span class=" bg-black rounded-full text-orange-500 text-xs font-bold px-3 py-2 leading-none flex items-center">$36.00</span>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Book Now
+                  </button>
                 </div>
               </div>
             </div>
