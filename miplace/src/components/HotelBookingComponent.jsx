026b1736af45323093f8cde1d3faab483f2235be
Rudransh <rudrcodes@gmail.com>
2023-09-14 18:32:29 +0530

    Added hotel details on the hotelInfo page and also added the logic to change currency based on the country location

--- miplace/src/components/HotelBookingComponent.jsx
+++ miplace/src/components/HotelBookingComponent.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const HotelBookingComponent = ({ hotelName, price, url, city, address, firstDay, secondDay,noOfDays }) => {
+const HotelBookingComponent = ({ currency, hotelName, price, url, city, address, firstDay, secondDay, noOfDays }) => {
+
   return (
     <>
-
       <div class=" border-solid rounded-lg border-2 border-{A1A1A1} mx-auto w-[49.375rem] h-[23.8125rem] ">
-
 
         <div class="flex mx-[1.5rem] mt-[2rem] mb-[1.5rem] py-[1.5rem] justify-between">
           <h1>Superior room - 1 double bed or 2 twin beds</h1>
-          <h2>₹ {price}/night</h2>
+          <h2>{price}/night</h2>
+          {/* <h2>{formatCurrency(price,currency)}/night</h2> */}
         </div>
 
         <div class="flex mx-[1.5rem] mb-[2.5rem]  w-[46.375rem] h-[5.9375rem] py-[1.5rem] rounded-lg border-solid border-2 border-{A1A1A1} gap-[2rem]">
