3ad0539209452e8f1b265acb3fb3c5defc49c6e5
Rudransh <rudrcodes@gmail.com>
2023-09-20 18:07:40 +0530

    Beautified Popular Place section

--- miplace/src/components/Popularplace.jsx
+++ miplace/src/components/Popularplace.jsx
@@ -12,31 +12,27 @@ export const Popularplace = () => {
     <div className='mt-[2.246875rem]'>
       <h1 className='mx-[4.8rem] font-mullish text-2xl font-bold'> Popular Places</h1>
 
-      <div id="popularsearch" class="flex justify-center items-center ">
+      <div id="popularsearch" className="flex justify-center items-center ">
 
 
 
-        <div class="p-6 flex flex-wrap items-center justify-center">
-
-
-          <div class="flex-shrink-0  relative overflow-hidde rounded-lg max-w-xs shadow-lg">
-
-            <div class="relative  flex items-center justify-center">
+        <div class="p-1 flex flex-wrap items-center justify-center hover:scale-105 transition ease-in-out delay-100 duration-300">
+          <div class="flex-shrink-0  relative overflow-hidde rounded-lg max-w-xs shadow-xl ">
+            <div class="relative  flex items-center justify-center p-3">
               <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
               <img
                 src={image}
                 alt="" />
-
             </div>
             <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
               <span class="block opacity-75 -mb-1"></span>
-              <div class="flex justify-between">
+              <div class="flex justify-center items-center">
                 <span class="block font-semibold text-xl font-mullish">Delhi</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   <a href="/searchResults">
                     Book Now
                   </a>
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
@@ -44,12 +40,15 @@ export const Popularplace = () => {
 
         </div>
 
-        <div class="p-12 flex flex-wrap items-center justify-center">
+
+
+
+        <div class="p-1 flex flex-wrap items-center justify-center hover:scale-105 transition ease-in-out delay-100 duration-300">
 
 
           <div class="flex-shrink-0  relative overflow-hidde rounded-lg max-w-xs shadow-lg">
 
-            <div class="relative  flex items-center justify-center">
+            <div class="relative  flex items-center justify-center p-3">
               <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
               <img
                 src={image1}
@@ -58,13 +57,13 @@ export const Popularplace = () => {
             </div>
             <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
               <span class="block opacity-75 -mb-1"></span>
-              <div class="flex justify-between">
+              <div class="flex justify-center items-center">
                 <span class="block font-semibold text-xl font-mullish">Jaipur</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   <a href="/searchResults">
                     Book Now
                   </a>
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
@@ -72,12 +71,12 @@ export const Popularplace = () => {
 
         </div>
 
-        <div class="p-12 flex flex-wrap items-center justify-center">
+        <div class="p-1 flex flex-wrap items-center justify-center hover:scale-105 transition ease-in-out delay-100 duration-300">
 
 
           <div class="flex-shrink-0  relative overflow-hidde rounded-lg max-w-xs shadow-lg">
 
-            <div class="relative  flex items-center justify-center">
+            <div class="relative  flex items-center justify-center p-3">
               <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
               <img
                 src={image2}
@@ -86,13 +85,13 @@ export const Popularplace = () => {
             </div>
             <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
               <span class="block opacity-75 -mb-1"></span>
-              <div class="flex justify-between">
+              <div class="flex justify-center items-center">
                 <span class="block font-semibold text-xl font-mullish">Gwalior</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   <a href="/searchResults">
                     Book Now
                   </a>
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
@@ -101,12 +100,12 @@ export const Popularplace = () => {
         </div>
 
 
-        <div class="p-12 flex flex-wrap items-center justify-center">
+        <div class="p-1 flex flex-wrap items-center justify-center hover:scale-105 transition ease-in-out delay-100 duration-300">
 
 
           <div class="flex-shrink-0  relative overflow-hidde rounded-lg max-w-xs shadow-lg">
 
-            <div class="relative  flex items-center justify-center">
+            <div class="relative  flex items-center justify-center p-3">
               <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3" ></div>
               <img
                 src={image3}
@@ -115,13 +114,13 @@ export const Popularplace = () => {
             </div>
             <div class=" text-brown inset-0 items-end px-6 pb-6 mt-6">
               <span class="block opacity-75 -mb-1"></span>
-              <div class="flex justify-between">
+              <div class="flex justify-center items-center">
                 <span class="block font-semibold text-xl font-mullish">Pune</span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                   <a href="/searchResults">
                     Book Now
                   </a>
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
